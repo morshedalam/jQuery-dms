7d4dc4b610bad4a0c87a841dbb912a94cd42985c
Morshed Alam <morshed201@gmail.com>
2013-06-26 20:24:49 +0600

    Deploy

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -8,19 +8,19 @@ CarrierWave.configure do |config|
     config.enable_processing = true
     config.fog_credentials = {
         :provider => 'AWS',
-        :aws_access_key_id => 'AKIAJQ5CKNWKQBR7DPBQ',
-        :aws_secret_access_key => 'sjQg8yq4nssf8cgn89NGdV8G/VBBbLKX4+Qq+UP8',
+        :aws_access_key_id => 'your aws access key',
+        :aws_secret_access_key => 'aws secret key',
         :region => 'us-east-1'
     }
 
     config.fog_public = false
-    config.fog_directory = 'jqdms'
+    config.fog_directory = 'file_cabinet'
+    #config.fog_host = 'https://s3.amazonaws.com'
+    #config.asset_host = 'http://JqueryDms.s3.amazonaws.com'
     config.fog_attributes = {'Cache-Control' => 'max-age=315576000'}
     config.fog_authenticated_url_expiration = 600
   else
-    #config.fog_host = 'https://s3.amazonaws.com'
     config.storage = :file
     config.enable_processing = false
-    config.asset_host = 'http://localhost:3002'
   end
 end
